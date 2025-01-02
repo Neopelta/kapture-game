f2c2079446fb4f08ec658f82eac828e49c4ac296
RPF <ronan.pluta@gmail.com>
2025-01-02 21:55:31 +0100

    feat: updating travel orders

--- include/commands/EndTurnCommand.h
+++ include/commands/EndTurnCommand.h
@@ -1,20 +1,13 @@
 #ifndef END_TURN_COMMAND_H
 #define END_TURN_COMMAND_H
 
-#include "../commands/command.h"
+#include "command.h"
 
 namespace kpt {
     class EndTurnCommand : public command {
     public:
-        EndTurnCommand() : command("end_turn",
-                              {"end", "fin"},
-                              "Termine le tour du joueur actuel") {}
-
-        void execute(const std::vector<std::string>& args) override {
-            if (!args.empty()) {
-                throw std::invalid_argument("La commande end ne prend pas d'arguments");
-            }
-        }
+        EndTurnCommand();
+        void execute(const std::vector<std::string>& args) override;
     };
 }
 
--- include/commands/stop.h
+++ include/commands/stop.h
@@ -1,20 +1,13 @@
 #ifndef STOP_COMMAND_H
 #define STOP_COMMAND_H
 
-#include "../commands/command.h"
+#include "command.h"
 
 namespace kpt {
     class StopCommand : public command {
     public:
-        StopCommand() : command("stop",
-                              {"s", "next"},
-                              "Passe à l'unité suivante") {}
-
-        void execute(const std::vector<std::string>& args) override {
-            if (!args.empty()) {
-                throw std::invalid_argument("La commande stop ne prend pas d'arguments");
-            }
-        }
+        StopCommand();
+        void execute(const std::vector<std::string>& args) override;
     };
 }
 
